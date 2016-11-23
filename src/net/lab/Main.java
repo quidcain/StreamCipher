f4b112e2a1f1cad1bc7754a3e1f7527b4e2059e0
quidcain <quidcain@gmail.com>
2016-11-23 13:29:14 +0300

    convenience rafactor

--- src/net/lab/Main.java
+++ src/net/lab/Main.java
@@ -37,7 +37,7 @@ public class Main {
                             bytesFromFile = new byte[bfrInSt.available()];
                             bfrInSt.read(bytesFromFile);
                         } catch(NullPointerException e){
-                            System.out.println("херня");
+                            JOptionPane.showMessageDialog(frame, "Не привязан файл", "Ошибка", JOptionPane.ERROR_MESSAGE);
                             return;
                         }catch (FileNotFoundException e) {
                             e.printStackTrace();
@@ -65,67 +65,70 @@ public class Main {
                 });
             }
         }
+        class MenuBar extends JMenuBar {
+            MenuBar() {
+                JMenu menuFile = new JMenu("Файл");
+                JMenuItem menuItemOpen = new JMenuItem("Открыть...");
+                JMenuItem menuItemBinaryDisplay = new JMenuItem("Вывести в двоичном виде");
+                menuFile.add(menuItemOpen);
+                menuFile.add(menuItemBinaryDisplay);
+                add(menuFile);
+                menuItemOpen.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        JFileChooser fc = new JFileChooser("/home/stoat/programming/IdeaProjects/StreamCipherBegin");
+                        if (fc.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
+                            File file = fc.getSelectedFile();
+                            fileName = file.getName();
+                            labelLinkedFile.setText("Файл: " + file.getName());
+                        }
+                    }
+                });
+                menuItemBinaryDisplay.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        byte[] bytesFromFile = null;
+                        try(BufferedInputStream bfrInSt = new BufferedInputStream(new FileInputStream(fileName))){
+                            bytesFromFile = new byte[bfrInSt.available()];
+                            bfrInSt.read(bytesFromFile);
+                        } catch(NullPointerException e1){
+                            System.out.println("херня");
+                            return;
+                        }catch (FileNotFoundException e1) {
+                            e1.printStackTrace();
+                            return;
+                        } catch (IOException e1) {
+                            e1.printStackTrace();
+                        }
+                        try(BufferedOutputStream bfrOutSt = new BufferedOutputStream(new FileOutputStream(fileName + "Binary"))){
+                            for (int i = 0; i < bytesFromFile.length; ++i) {
+                                for (int j = 0; j < 8; ++j) {;
+                                    bfrOutSt.write((bytesFromFile[i] >> j & 1) == 1 ? '1' : '0');
+                                }
+                            }
+                        }catch (FileNotFoundException e1) {
+                            System.out.println("huevo" + e1.getMessage());
+                            return;
+                        } catch (IOException e1) {
+                            e1.printStackTrace();
+                        }
+                    }
+                });
+            }
+        }
+
         mainPanel = new JPanel(new CardLayout());
         mainPanel.add(new SingleLfsrPanel(), "singleLfsr");
         //mainPanel.add(new AfterAuthPanel(), "Smth another");*/
         frame = new JFrame("Потоковое шифрование");
 
-        JMenuBar menuBar = new JMenuBar();
-        JMenu menuFile = new JMenu("Файл");
-        JMenuItem menuItemOpen = new JMenuItem("Открыть...");
-        JMenuItem menuItemBinaryDisplay = new JMenuItem("Вывести в двоичном виде");
-        menuFile.add(menuItemOpen);
-        menuFile.add(menuItemBinaryDisplay);
-        menuBar.add(menuFile);
-        menuItemOpen.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFileChooser fc = new JFileChooser("/home/stoat/programming/IdeaProjects/StreamCipherBegin");
-                //fc.setCurrentDirectory(new File(""));
-                if (fc.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
-                    File file = fc.getSelectedFile();
-                    fileName = file.getName();
-                    labelLinkedFile.setText("Файл: " + file.getName());
-                }
-            }
-        });
-        menuItemBinaryDisplay.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                byte[] bytesFromFile = null;
-                try(BufferedInputStream bfrInSt = new BufferedInputStream(new FileInputStream(fileName))){
-                    bytesFromFile = new byte[bfrInSt.available()];
-                    bfrInSt.read(bytesFromFile);
-                } catch(NullPointerException e1){
-                    System.out.println("херня");
-                    return;
-                }catch (FileNotFoundException e1) {
-                    e1.printStackTrace();
-                    return;
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                }
-                try(BufferedOutputStream bfrOutSt = new BufferedOutputStream(new FileOutputStream(fileName + "Binary"))){
-                    for (int i = 0; i < bytesFromFile.length; ++i) {
-                        for (int j = 0; j < 8; ++j) {;
-                            bfrOutSt.write((bytesFromFile[i] >> j & 1) == 1 ? '1' : '0');
-                        }
-                    }
-                }catch (FileNotFoundException e1) {
-                    System.out.println("huevo" + e1.getMessage());
-                    return;
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                }
-            }
-        });
-        frame.setJMenuBar(menuBar);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(290, 330);
         frame.setMinimumSize(new Dimension(290, 330));
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        frame.setJMenuBar(new MenuBar());
         frame.add(mainPanel);
     }
     public static void main(String[] args) {
